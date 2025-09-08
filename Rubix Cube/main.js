51b15a86c3ad8c8bbbc8dfd496ba37417df5adba
Sarthak <sarthakss898@gmail.com>
2025-09-08 19:19:23 +0530

    Scale borders by 1.001
    Borders were coinciding with the cube geometries, hence appearing broken.

--- Rubix Cube/main.js
+++ Rubix Cube/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true});
 
 function getRandomColor() {
   const colors = ['red', 'green', 'blue', 'yellow', 'white', 'orange']
@@ -29,7 +29,7 @@ for (let i = 0; i < 3; i++) {
       const edgeGeometry = new THREE.EdgesGeometry(geometry);
       const edgeMaterial = new THREE.LineBasicMaterial({color:0x000000});
       const edges = new THREE.LineSegments(edgeGeometry, edgeMaterial);
-
+      edges.scale.setScalar(1.001);
       scene.add( cube );
       cube.add( edges );
       cube.position.set(i,j,k)
