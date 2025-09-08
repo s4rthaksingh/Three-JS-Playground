5f7fb854ef0850d4d64d54daaa0d1253fde71fa4
Sarthak <sarthakss898@gmail.com>
2025-09-08 16:28:24 +0530

    Show cubes random colors

--- Rubix Cube/main.js
+++ Rubix Cube/main.js
@@ -1,25 +1,45 @@
 import * as THREE from 'three';
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 10, 10, 10 );
+controls.update();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    for (let k = 0; k < 3; k++) {
+      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      const material = new THREE.MeshBasicMaterial( { color: getRandomColor() } );
+      const cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+      cube.position.set(i,j,k)
+    }
+  }
+}
 
-camera.position.z = 5;
+
+
 
 function animate() {
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  
+	controls.update();
   renderer.render( scene, camera );
 
 }
